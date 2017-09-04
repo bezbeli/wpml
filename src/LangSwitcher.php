9fe6d657a275bac64a498ff6352aa2a49897d070
bezbeli <bezbeli@gmail.com>
2017-09-04 22:06:31 +0200

    add class to langswitcher

--- src/LangSwitcher.php
+++ src/LangSwitcher.php
@@ -2,25 +2,28 @@
 
 namespace Bezbeli\WPML;
 
-function LangSwitcher()
+class LangSwitcher
 {
-    if (function_exists('icl_get_languages')) {
-        $languages = icl_get_languages('skip_missing=1');
-        if (1 < count($languages)) {
-            $ll_current = $languages[ICL_LANGUAGE_CODE]['native_name'];
-            foreach ($languages as $l) {
-                if (!$l['active']) {
-                    $items = '<li class="menu-item custom-switcher"><a class="nav-link" href="'
-                    . $l['url'] . '"><img src="'
-                    . $l['country_flag_url']
-                    . '" height="12" alt="'
-                    . $l['language_code']
-                    . '" width="18" /></a></li>';
+    public function show()
+    {
+        if (function_exists('icl_get_languages')) {
+            $languages = icl_get_languages('skip_missing=1');
+            if (1 < count($languages)) {
+                $ll_current = $languages[ICL_LANGUAGE_CODE]['native_name'];
+                foreach ($languages as $l) {
+                    if (!$l['active']) {
+                        $items = '<li class="menu-item custom-switcher"><a class="nav-link" href="'
+                        . $l['url'] . '"><img src="'
+                        . $l['country_flag_url']
+                        . '" height="12" alt="'
+                        . $l['language_code']
+                        . '" width="18" /></a></li>';
+                    }
                 }
             }
         }
+        return $items;
     }
-    return $items;
-}
 
 // add_filter('wp_nav_menu_items', __NAMESPACE__ . '\\lang_dropdown', 10, 2);
+}
